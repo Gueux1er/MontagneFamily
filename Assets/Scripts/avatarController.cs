982c9aae65886a0196945e8f54b0bd32f59a9b37
sail741 <leo.mullot@gmail.com>
2018-01-26 20:38:02 +0100

    Default scene

---
+++ Assets/Scripts/avatarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avatarController : MonoBehaviour
+{
+
+    public float moveSpeed = 2;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(Vector3.right * h);
+    }
+}
