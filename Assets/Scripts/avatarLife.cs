779030b004e73e43f8c35046800b2891a582b2bc
sail741 <leo.mullot@gmail.com>
2018-01-26 23:05:22 +0100

    Added timer and life bar

---
+++ Assets/Scripts/avatarLife.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class avatarLife : MonoBehaviour
+{
+
+    public int startingLife = 5;
+    public int maxLife = 5;
+    public int currentLife;
+    public Image lifeImageEvnt;
+    public float flashSpeed = 5f;
+    public Color damagedColour = new Color(1f, 0f, 0f, 0.3f);
+    public Color healedColour = new Color(0f, 1f, 0f, 0.3f);
+
+    public Image[]  hearts;
+
+
+    avatarController avatarController;
+    bool isDeadOld;
+    bool damaged;
+    bool healed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        avatarController = GetComponent<avatarController>();
+        currentLife = startingLife;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            lifeImageEvnt.color = damagedColour;
+        } else if(healed)
+        {
+            lifeImageEvnt.color = healedColour;
+        }
+        else
+        {
+            lifeImageEvnt.color = Color.Lerp(lifeImageEvnt.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        currentLife = 3;
+        UpdateHearts();
+        damaged = false;
+        healed = false;
+    }
+
+    public void TakeDamage(int value)
+    {
+        damaged = true;
+        currentLife -= value;
+
+        if(currentLife <= 0)
+        {
+            // TODO : death
+        }
+
+        UpdateHearts();
+    }
+
+    public void TakeHeal(int value)
+    {
+        healed = true;
+        currentLife += value;
+
+        if(currentLife > maxLife)
+        {
+            currentLife = maxLife;
+        }
+
+        UpdateHearts();
+    }
+
+    private void UpdateHearts()
+    {
+
+        for(int i=0; i<maxLife-currentLife; i++)
+        {
+            // TODO : set with correct empty heart image
+            hearts[i].color = new Color(0f, 0f, 0f, 1f);
+        }
+        for(int i=maxLife-currentLife; i<maxLife; i++)
+        {
+            // TODO : set with correct heart image
+            hearts[i].color = new Color(1f, 0f, 0f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/avatarTimeline.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class avatarTimeline : MonoBehaviour {
+
+    public float currentTime;
+    public Slider timeSlider;
+    public Image timedImage;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.3f);
+
+    avatarController avatarController;
+    bool isDeadOld;
+    bool timed;
+
+	// Use this for initialization
+	void Start () {
+
+        avatarController = GetComponent<avatarController>();
+        currentTime = 0f; ;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+        if (((int)currentTime) == 70 || ((int)currentTime) == 30)
+        {
+            timed = true;
+        }
+
+        if(currentTime <= 0)
+        {
+            // TODO : death
+        }
+
+        if (timed)
+        {
+            timedImage.color = flashColour;
+        } else
+        {
+            timedImage.color = Color.Lerp(timedImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        timed = false;
+        timeSlider.value = currentTime;
+	}
+}
